ee48da740d62ac4cc9a757a05257c127f529cae0
Kyle Mistele <kyle@humanlayer.dev>
2026-08-07 15:41:05 -0700

    Update Riptide Electron dev alias to 0.1.1

--- Casks/riptide-electron-dev.rb
+++ Casks/riptide-electron-dev.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 cask "riptide-electron-dev" do
-  version "0.1.0"
-  sha256 "2664b1cc4d09907a8e2893e40c99b74e381af5ed9a606043b11ef1f28b771dea"
+  version "0.1.1"
+  sha256 "d989578ba10f5c837424e4848530f8390b629a23a2ecf84afb4a49ce10e74fa4"
 
-  url "https://github.com/humanlayer/homebrew-humanlayer/releases/download/humanlayer-electron-dev-v0.1.0/HumanLayer-Dev.Electron-0.1.0-arm64.dmg",
+  url "https://github.com/humanlayer/homebrew-humanlayer/releases/download/humanlayer-electron-dev-v0.1.1/HumanLayerElectronDev-0.1.1-arm64.dmg",
       verified: "github.com/humanlayer/homebrew-humanlayer/"
-  name "HumanLayer-Dev Electron"
+  name "HumanLayerElectronDev"
   desc "Development Electron build of HumanLayer pointing at the dev environment"
   homepage "https://humanlayer.dev/"
 
@@ -15,8 +15,8 @@ cask "riptide-electron-dev" do
   depends_on arch: :arm64
   depends_on macos: :big_sur
 
-  app "HumanLayer-Dev Electron.app"
-  binary "#{appdir}/HumanLayer-Dev Electron.app/Contents/Resources/bin/riptided", target: "riptided-electron-dev"
+  app "HumanLayerElectronDev.app"
+  binary "#{appdir}/HumanLayerElectronDev.app/Contents/Resources/bin/riptided", target: "riptided-electron-dev"
 
   zap trash: [
     "~/.humanlayer/riptide/electron/dev",
